acbf5368bb700d3919dfc79300c26b5580f90e73
issstasevich <pravitel2015ify@gmail.com>
2023-06-17 16:30:28 +0300

    Fix pyinstaller

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from calculator import calculator
+def get_ops():
+    return {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y,
+    }
 
-from rand import rand
+
+def calculator():
+    ops = get_ops()
+
+    try:
+        number1 = float(input('Введите A: '))
+        op = input('Введите операцию: ')
+        number2 = float(input('Введите B: '))
+
+        res = ops[op](number1, number2)
+    except ValueError:
+        print('Неверный формат числа!')
+        return
+    except KeyError:
+        print('Неизвестная операция!')
+        return
+    except ZeroDivisionError:
+        print('Деление на ноль запрещено!')
+        return
+
+    print('Результат:')
+    print(f'{number1} {op} {number2} = {res}')
 
 
 def main():
-    rand()
     calculator()
 
 
--- rand.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-
-
-def rand():
-    number = randint(1, 1_000_000)
-    print(f'Случайное число: {number}')
